f14cdbdcd613224f32e580df6a18a98f446ea301
lokeshreddy1999 <tlokeshreddy1999@gmail.com>
2020-04-27 00:21:20 +0530

    heraphiri added

---
+++ heraphiri.c
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define MOD 1e9+7
+using namespace std;
+#define  pp push_back
+#define  po pop_back
+#define  mp make_pair
+#define clr(a) memset(a,0,sizeof(a))
+#define neg(a) memset(a,-1,sizeof(a))
+#define fas(a) memset(a,false,sizeof(a))
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define debug1(x) cout<<#x<<": "<<x<<" "
+#define rev(x) reverse(x.begin(), x.end())
+#define int long long 
+#define F first
+#define S second
+//int a[2000];
+void printvector(std::vector<int> v){  for (int i = 0; i < v.size(); ++i){cout<<v[i]<<" ";}cout<<""<<endl;}
+void printarray(int a[],int n){for (int i = 0; i < n; ++i){cout<<a[i]<<" ";}cout<<""<<endl;}
+ 
+ 
+
+ 
+
+void  solve()
+{
+  
+
+}
+
+
+
+
+ int32_t main() 
+{
+
+  #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+  #endif
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+    
+   solve();
+
+return 0;
+
+}
